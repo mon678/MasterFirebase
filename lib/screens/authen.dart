dc4a2fd359906eb14c3e772dc8b2476eb544573b
mon <mon@gmail.com>
2019-04-29 15:33:36 +0700

    color gra

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -8,16 +9,41 @@ class Authen extends StatefulWidget {
 
 class _AuthenState extends State<Authen> {
   Widget showAppName() {
-    return Text('Master Flutter');
+    return Text(
+      'Master Flutter',
+      style: TextStyle(
+          fontFamily: 'Itim-Regular',
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue[800]),
+    );
+  }
+
+  Widget showlogo() {
+    return Image.asset('images/logo.png');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.white],begin: Alignment(-1, 1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
-        child: showAppName(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 200.0,
+              height: 100.0,
+              child: showlogo(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50.0),
+              child: showAppName(),
+            ),
+          ],
+        ),
       ),
     );
   }
