7c1dd51248f9f99f7fa878b928b5516a354e069e
mon <mon@gmail.com>
2019-04-29 16:38:48 +0700

    fins291

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/cupertino.dart' as prefix0;
 import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' as prefix1;
 import 'package:flutter/rendering.dart';
 
 class Authen extends StatefulWidget {
