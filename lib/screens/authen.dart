9164f5149ef4287780508bb321f6bde994e18138
mon <mon@gmail.com>
2019-04-29 14:10:11 +0700

    Create Method Return

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -6,8 +7,18 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text('Master Flutter');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('StateFulWidget');
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 100.0),
+        alignment: Alignment(0, -1),
+        child: showAppName(),
+      ),
+    );
   }
 }
